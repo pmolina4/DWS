486cc0427080fc623b83ddeb24fa9d78018ff5cd
pmolina4 <pablomolinaconde4@gmail.com>
2022-10-25 11:57:37 +0200

    array

---
+++ Arrays/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arrays</title>
+</head>
+
+<body>
+    <?php
+
+    $videojuegos1 = array(
+        "juego1" => "Cyberpunk 2077",
+        "juego2" => "Minecraft",
+        "juego3" => "My little pony"
+    );
+    print_r($videojuegos1);
+
+    $consolas = [
+        "consola1" => "PS1",
+        "cosnola2" => "PS2",
+        "consola3" => "PS3"
+    ];
+    print_r($consolas);
+
+
+    $dni = [
+        "26257287G" => "Pablo",
+        "26258887H" => "Lorenzo",
+        "36325897O" => "Alvaro"
+    ];
+    print_r($dni);
+
+    ?>
+</body>
+
+</html>
