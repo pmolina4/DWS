8b4d7be4112f514dfbde87712ba0e8e4f8991284
pmolina4 <pablomolinaconde4@gmail.com>
2022-11-04 18:34:23 +0100

    link de navbar

--- BD/public/header.php
+++ BD/public/header.php
@@ -1,13 +1,12 @@
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="http://localhost/DWS/BD/public">Cenec Moda</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">Cenec Moda</a>
-        <a class="nav-link" href="#">ROPA</a>
+        <a class="nav-link" href="http://localhost/DWS/BD/public/ropa">ROPA</a>
         <a class="nav-link" href="#">CLIENTES</a>
       </div>
     </div>
