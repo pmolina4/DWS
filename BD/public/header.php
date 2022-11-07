a50c03a1cd93f01683db89498c266c82d5af92d0
pmolina4 <pablomolinaconde4@gmail.com>
2022-11-07 09:40:14 +0100

    Clientes bd
    clientes bd

--- BD/public/header.php
+++ BD/public/header.php
@@ -1,5 +1,10 @@
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
+    <div>
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door" viewBox="0 0 16 16">
+        <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z" />
+      </svg>
+    </div>
     <a class="navbar-brand" href="http://localhost/DWS/BD/public">Cenec Moda</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -7,7 +12,7 @@
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
         <a class="nav-link" href="http://localhost/DWS/BD/public/ropa">ROPA</a>
-        <a class="nav-link" href="#">CLIENTES</a>
+        <a class="nav-link" href="http://localhost/DWS/BD/public/ropa/insertar_cliente.php">CLIENTES</a>
       </div>
     </div>
   </div>
---
+++ BD/public/ropa/insertar_cliente.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <title>Nuevo cliente</title>
+</head>
+
+<body>
+    <?php
+    //importamos fichero para la conexion de la bd
+    require '../util/databases.php';
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $usuario = $_POST["usuario"];
+        $nombre = $_POST["nombre"];
+        $apellido1 = $_POST['apellido1'];
+        $apellido2 = $_POST["apellido2"];
+        $fechaNacimiento = $_POST['fechaN'];
+        //comprobamos que los datos se han introducido en el formulario
+        if (!empty($nombre) && !empty($usuario) && !empty($apellido1) && !empty($apellido2) && !empty($fechaNacimiento)) {
+            //creamos la sentencia SQL
+            $sql = "INSERT INTO cliente (usuario, nombre, apellido1, apellido2, fecha_nacimiento)
+                    VALUES('$usuario', '$nombre','$apellido1','$apellido2','$fechaNacimiento')";
+            //si la sentencia se ejecuta correctamente mostramos ok si no pues no
+            if ($conexion->query($sql) == "TRUE") {
+                echo "<p>Cliente Insertado</p>";
+            } else {
+                echo "<p>Error al insertar</p>";
+            }
+        }
+    }
+    ?>
+
+    <div class="container">
+        <?php require '../header.php' ?>
+        <br>
+        <h1>Nuevo Cliente</h1>
+
+        <div class="row">
+            <div class="col-6">
+                <form action="" method="POST">
+                    <label class="form-label">Usuario</label>
+                    <input type="text" class="form-control" name="usuario">
+                    <br>
+                    <label class="form-label">Nombre</label>
+                    <input type="text" class="form-control" name="nombre">
+                    <br>
+                    <label class="form-label">Apellido 1</label>
+                    <input type="text" class="form-control" name="apellido1">
+                    <br>
+                    <label class="form-label">Apellido 2</label>
+                    <input type="text" class="form-control" name="apellido2">
+                    <br>
+                    <label class="form-label">Fecha Nacimiento</label>
+                    <input type="date" class=" form-control sm-form-control" name="fechaN">
+                    <br>
+                    <button class="btn btn-primary" type="submit">Crear</button>
+                    <a class="btn btn-secondary" href="index.php">Volver</a>
+                </form>
+            </div>
+        </div>
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+
+</html>
